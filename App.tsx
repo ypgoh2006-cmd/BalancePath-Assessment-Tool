5293e8028fd0214f10d90d8ed1e57b06a7c1ac50
ypgoh2006-cmd <ypgoh2006@gmail.com>
2025-12-14 22:14:26 +0800

    remove React from app.tsx line 1

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import WelcomeScreen from './components/WelcomeScreen';
 import Questionnaire from './components/Questionnaire';
 import ResultsDashboard from './components/ResultsDashboard';
